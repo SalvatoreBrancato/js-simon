304a97293df9eb9d5f550f87300c6106a413f423
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-04-06 12:28:01 +0200

    Struttura_definitiva

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,9 @@
 let arrayNumeriPc = []
 for (let i = 0; i <= 4; i++) {
     let numeroRandom = Math.floor(Math.random() * 10) + 1;
+    arrayNumeriPc.push(numeroRandom)
     
-    arrayNumeriPc.push([numeroRandom])
-    
-    
-    document.querySelector("#numeroPC").innerHTML += `<div class="square d-flex justify-content-center align-items-center"><span class="numeroPC">${numeroRandom}</span></div>`
-   
-      
+    document.querySelector("#numeroPC").innerHTML += `<div class="square d-flex justify-content-center align-items-center"><span class="numeroPC">${numeroRandom}</span></div>`   
 }
     console.log(arrayNumeriPc)
 
@@ -25,31 +21,53 @@ for (let i = 0; i <= 4; i++) {
 let arrayNumeriUtente = []
 
 setTimeout(avviaPrompt, 3500)
+
 function avviaPrompt(){
 
     for (let k = 0; k <= 4; k++) {
-        let numeroUtente = parseInt(prompt("inserisci il numero"))
-        arrayNumeriUtente.push([numeroUtente])
-        document.querySelector("#numeroUtente").innerHTML += `<div class="square d-flex justify-content-center align-items-center"><span>${numeroUtente}</span></div>`
+        let numeroPrompt = parseInt(prompt("inserisci il numero"))
+        if (numeroPrompt <= 10) {
+            let numeroUtente = numeroPrompt
+            arrayNumeriUtente.push(numeroUtente)
+            document.querySelector("#numeroUtente").innerHTML += `<div class="contenitore${[k]} square d-flex justify-content-center align-items-center"><span>${numeroUtente}</span></div>`
+    
+            const list = document.querySelectorAll(".numeroPC");
+            for (let z = 0; z < list.length; z++) {
+                list[z].classList.remove("d-none")
+            }
 
-        const list = document.querySelectorAll(".numeroPC");
-        for (let z = 0; z < list.length; z++) {
-            list[z].classList.remove("d-none")
+            
+        }else{
+            alert("il numero è magiore di 10")
+            k--            
         }
-
-    }
-    if (arrayNumeriPc.includes(arrayNumeroPc)) {
-        console.log("hai vinto")
-    }else{
-        console.log("hai perso")
+            
     }
 
+    let arrayNumeriEsatti = []
+    let punteggio;
+    for (let i = 0; i < arrayNumeriUtente.length; i++) {
+        
+        let contenitore = document.querySelector(`.contenitore${[i]}`)
+        if (arrayNumeriPc.includes(arrayNumeriUtente[i]) && arrayNumeriPc[i]== arrayNumeriUtente[i]) {
+            arrayNumeriEsatti.push(arrayNumeriUtente[i])
+            contenitore.classList.add("bg-success")
+            
+        }else if(arrayNumeriPc.includes(arrayNumeriUtente[i])){
+            arrayNumeriEsatti.push(arrayNumeriUtente[i])
+            contenitore.classList.add("bg-warning")
+        
+        }else{
+            contenitore.classList.add("bg-danger")
+           
+        }
+        punteggio = arrayNumeriEsatti.lenght
+    }
+    
+    console.log(arrayNumeriEsatti.length)
+    setTimeout(avviaAlert, 1000)
+    
+    function avviaAlert(){
+        alert("Hai totalizzato" + " " + arrayNumeriEsatti.length)}
+    
 }
-
-
-
-console.log(arrayNumeriUtente)
-
-
-
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
